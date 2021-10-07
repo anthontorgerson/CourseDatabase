026ebea93aa6fa91237fb04da53d2281005e3ba2
Dylan <djthomas4@dmacc.edu>
2021-10-07 17:51:19 -0500

    First Commit

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * @author Dylan Thomas - djthomas4
+ * CIS175 - Fall 2021
+ * Oct 7, 2021
+ */
+public class Student {
+
+	/**
+	 * 
+	 */
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
