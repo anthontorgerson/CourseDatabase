ada8f0a28ab932dfcdbeb416564163da1ab463e6
Dylan <djthomas4@dmacc.edu>
2021-10-15 04:21:09 -0500

    Minor Changes
    Changed cascade type in CourseDetails.java, added findStudent() in StudentHelper, changed table name to 'students' in Student.java

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="entries")
+@Table(name="students") // Changed table name to students
 public class Student {
 	// attributes
 	@Id
