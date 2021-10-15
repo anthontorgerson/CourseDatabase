984582fe3f27fecf0d78f5c1953a93fd2cce6106
Dylan <djthomas4@dmacc.edu>
2021-10-14 21:45:01 -0500

    Created AddStudentServlet.java, ViewAllStudentsServlet.java, and student-list.jsp

---
+++ src/main/java/controller/ViewAllStudentsServlet.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewAllStudentsServlet
+ */
+@WebServlet("/ViewAllStudentsServlet")
+public class ViewAllStudentsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewAllStudentsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		StudentHelper dao = new StudentHelper();
+		request.setAttribute("allStudents", dao.showAllEntries());
+		String path = "/student-list.jsp";
+				
+		if(dao.showAllEntries().isEmpty()){
+			path = "/index.html";
+		}
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
